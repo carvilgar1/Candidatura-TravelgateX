203872fc14d7bf76a0a210fef7819ab9fc5008cb
carvilgar1 <carvilgar1@alum.us.es>
2022-09-07 02:35:08 +0200

    MAJOR CODE REFACTORIZATION AND BUGS ELIMINATION

--- src/punto_dos.py
+++ src/punto_dos.py
@@ -2,7 +2,17 @@ from punto_uno import punto_uno
 from punto_uno import MealPlan 
 from punto_uno import RoomType
 
-def punto_dos(budget):
+NIGHTS_IN_MALAGA = 3
+NIGHTS_IN_CANCUN = 5
+ROOM_TYPE = RoomType.SUITE
+
+def price_calculator(nights_in_malaga,malaga_room, nights_in_cancun, cancun_room) -> int: 
+        '''Returns the total cost of the itinenary. If no itinenary then returns 0'''
+        if malaga_room == None:
+            return 0
+        return nights_in_malaga*malaga_room['price'] + nights_in_cancun*cancun_room['price']
+
+def find_best_itinerary_algorithm(json_malaga, json_cancun, budget):
     '''
     Find the best itinerary through a greedy algorithm that follows a heuristic that tries to maximize the 
     budget for the hotel in Malaga and finds any option that meets the requirements for the hotel in Cancun.
@@ -10,30 +20,35 @@ def punto_dos(budget):
     @param budget: Budget specified by the client in the request to the endpoint.
     @return: JSON formatted dictionary that contains the best itinerary for the required budget.
     '''
-    def price_predicate(nights_in_malaga,malaga_room, nights_in_cancun, cancun_room, total_budget) -> bool: 
-        '''Returns true if the total price is within the budget limits and false otherwise'''
-        return nights_in_malaga*malaga_room['price'] + nights_in_cancun*cancun_room['price'] <= total_budget
-    
+    price_predicate = lambda price: price <= budget
+
+    best_combination = (None, None)
+
+    for malaga_room in json_malaga['rooms']:
+        if malaga_room['room_type'] == ROOM_TYPE:
+            if not best_combination[0] == None:
+                #Lets try to find a preliminar valid path from starting point. function returns a (null, null) tuple if cant find a valid solution, this
+                #is the duty of this if statement.
+                if not(best_combination[0]['price'] < malaga_room['price']):
+                        continue
+            else:
+                for cancun_room in json_cancun['rooms']:
+                    if not(cancun_room['meal_plan'] == MealPlan.ACCOMMODATION_AND_BREAKFAST and cancun_room['room_type'] == ROOM_TYPE and 
+                            price_predicate(price_calculator(NIGHTS_IN_MALAGA, malaga_room, NIGHTS_IN_CANCUN, cancun_room))):
+                        continue
+                    best_combination = (malaga_room, cancun_room)
+                    break
+    return best_combination
+
+def punto_dos(budget):
     json_hotel = punto_uno()
    
     json_malaga = next(filter(lambda hotel: hotel['city'] == 'Malaga', json_hotel['hotels']))
-    json_cacun = next(filter(lambda hotel: hotel['city'] == 'Cancun', json_hotel['hotels']))
-    
-    best_combination = (None, )
+    json_cancun = next(filter(lambda hotel: hotel['city'] == 'Cancun', json_hotel['hotels']))
 
-    for malaga_room in json_malaga['rooms']:
-        if not best_combination[0] == None:
-            if not(best_combination[0]['price'] < malaga_room['price'] and malaga_room['room_type'] == RoomType.SUITE):
-                continue
-        for cancun_room in json_cacun['rooms']:
-            if not(cancun_room['meal_plan'] == MealPlan.ACCOMMODATION_AND_BREAKFAST and cancun_room['room_type'] == RoomType.SUITE and 
-                    price_predicate(3, malaga_room, 5, cancun_room, budget)):
-                continue
-            best_combination = (malaga_room, cancun_room)
-            break
-           
-
+    best_combination = find_best_itinerary_algorithm(json_malaga, json_cancun, budget)
     json_malaga['rooms'] = best_combination[0]
-    json_cacun['rooms'] = best_combination[1]
+    json_cancun['rooms'] = best_combination[1]
 
-    return {'hotels' : [json_malaga, json_cacun]}
+    return {'nights_in_malaga': NIGHTS_IN_MALAGA, 'nights_in_cancun': NIGHTS_IN_CANCUN, 
+                'total_price': price_calculator(NIGHTS_IN_MALAGA, best_combination[0], NIGHTS_IN_CANCUN, best_combination[1]), 'hotels' : [json_malaga, json_cancun]}
--- src/punto_uno.py
+++ src/punto_uno.py
@@ -16,18 +16,12 @@ class MealPlan(str, Enum):
     ACCOMMODATION_AND_BREAKFAST = 'ACCOMMODATION_AND_BREAKFAST'
 
 #API endpoints
-url_api_hoteles_atalaya = 'http://www.mocky.io/v2/5e4a7e4f2f00005d0097d253'
-url_api_resort_hoteles = 'http://www.mocky.io/v2/5e4e43272f00006c0016a52b'
-url_rooms_information = 'https://run.mocky.io/v3/132af02e-8beb-438f-ac6e-a9902bc67036'
-url_meal_plans_information = 'http://www.mocky.io/v2/5e4a7e282f0000490097d252'
-url_available_regimens = 'http://www.mocky.io/v2/5e4a7dd02f0000290097d24b'
+URL_API_HOTELES_ATALAYA = 'http://www.mocky.io/v2/5e4a7e4f2f00005d0097d253'
+URL_API_RESORT_HOTELES = 'http://www.mocky.io/v2/5e4e43272f00006c0016a52b'
+URL_ROOMS_INFORMATION = 'https://run.mocky.io/v3/132af02e-8beb-438f-ac6e-a9902bc67036'
+URL_MEAL_PLANS_INFORMATION = 'http://www.mocky.io/v2/5e4a7e282f0000490097d252'
+URL_AVAILABLE_REGIMENS = 'http://www.mocky.io/v2/5e4a7dd02f0000290097d24b'
 
-#JSON formatted gotten from the previous urls are converted to Python object
-json_api_hoteles_atalaya = json.loads(urlopen(url_api_hoteles_atalaya).read()) 
-json_api_resort_hoteles = json.loads(urlopen(url_api_resort_hoteles).read())
-json_rooms_information = json.loads(urlopen(url_rooms_information).read())
-json_meal_plans_information = json.loads(urlopen(url_meal_plans_information).read())
-json_available_regimens = json.loads(urlopen(url_available_regimens).read())
 
 def room_type_normalization(room_str) -> RoomType:
     '''
@@ -53,59 +47,75 @@ def meal_plans_normalization(meal_plan_str) -> MealPlan:
     else:
         return MealPlan.ACCOMMODATION_AND_BREAKFAST
 
-def atalaya_hotel_api_standardization() -> None:
+def atalaya_hotel_api_standardization() -> dict:
     '''
     This function takes the data provided by url_rooms_information, iterates over it
     and creates a temporary dictionary with room information and meal plans (provided by json_meal_plans_information). Finally, the new
     dictionary is embedded along with the rest of the hotel information.
     '''
-    for room_information in json_rooms_information['rooms_type']:
-        for available_hotel in room_information['hotels']:
-            for meal_plan in json_meal_plans_information['meal_plans']:
-                room = dict()
-                room['name'] = room_information['name']
-                room['room_type']  = room_type_normalization(room_information['code'])
-                
-                for room_price in meal_plan['hotel'][available_hotel]: 
-                    if room['room_type'] == room_type_normalization(room_price['room']):
-                        room['meal_plan']  = meal_plans_normalization(meal_plan['code'])
-                        room['price']  = room_price['price']
-                
-                for hotel in json_api_hoteles_atalaya['hotels']:
-                    if hotel['code'] == available_hotel:
-                        if 'rooms' in hotel:
-                            hotel['rooms'].append(room)
-                        else:
-                            hotel['rooms'] = [room]
+    json_api_hoteles_atalaya = json.loads(urlopen(URL_API_HOTELES_ATALAYA).read()) 
+    json_rooms_information = json.loads(urlopen(URL_ROOMS_INFORMATION).read())
+    json_meal_plans_information = json.loads(urlopen(URL_MEAL_PLANS_INFORMATION).read())
 
+    result = {'hotels' : []}
+    for hotel in json_api_hoteles_atalaya['hotels']:
+        formatted_hotel = dict()
+        formatted_hotel['code'] = hotel['code']
+        formatted_hotel['name'] = hotel['name']
+        formatted_hotel['city'] = hotel['city']
+        formatted_hotel['rooms'] = []
+        for room_information in json_rooms_information['rooms_type']:
+            for available_hotel in room_information['hotels']:
+                if hotel['code'] == available_hotel:
+                    for meal_plan in json_meal_plans_information['meal_plans']:
+                        room = dict()
+                        room['name'] = room_information['name']
+                        room['room_type']  = room_type_normalization(room_information['code'])
+                        
+                        for room_price in meal_plan['hotel'][available_hotel]: 
+                            if room['room_type'] == room_type_normalization(room_price['room']):
+                                room['meal_plan']  = meal_plans_normalization(meal_plan['code'])
+                                room['price']  = room_price['price']
+                        formatted_hotel['rooms'].append(room)
+        result['hotels'].append(formatted_hotel)
+    return result
 
-def resort_hotel_api_standardization() -> None:
+def resort_hotel_api_standardization() -> dict:
     '''
     This function inserts the missing information about meal plans of hotel resort API, which
     is found in url_available_regimens variable, to meet with company's API standard.
     '''
+    json_api_resort_hoteles = json.loads(urlopen(URL_API_RESORT_HOTELES).read())
+    json_available_regimens = json.loads(urlopen(URL_AVAILABLE_REGIMENS).read())
+    
+    result = {'hotels' : []}
     for hotel in json_api_resort_hoteles['hotels']:
-        hotel['city'] = hotel.pop('location')
-        for room in hotel['rooms']:
-            room['room_type']  = room_type_normalization(room['code'])
-            del room['code']
+        formatted_hotel = dict()
+        formatted_hotel['code'] = hotel['code']
+        formatted_hotel['name'] = hotel['name']
+        formatted_hotel['city'] = hotel['location']
+        formatted_hotel['rooms'] = []
+        for room_information in hotel['rooms']:
+            room = dict()
+            room['name'] = room_information['name']
+            room['room_type']  = room_type_normalization(room_information['code'])
             for regime in json_available_regimens['regimenes']:
                 if (room['room_type'] == room_type_normalization(regime['room_type']) 
                         and regime['hotel'] == hotel['code']):
                     room['meal_plan']  = meal_plans_normalization(regime['code'])
                     room['price']  = regime['price']
+            formatted_hotel['rooms'].append(room)
+        result['hotels'].append(formatted_hotel)
+    return result
 
 def punto_uno() -> dict:
     '''
     This function returns the standardized JSON demanded by the Company.
-    It also create a local file with .json extension.
     '''
-    atalaya_hotel_api_standardization()
-    resort_hotel_api_standardization()
-    json_api_hoteles_atalaya['hotels'].extend(json_api_resort_hoteles['hotels'])
-    with open('punto_uno.json', 'w+') as f:
-        f.write(json.dumps(json_api_hoteles_atalaya, indent=1))
-    return json_api_hoteles_atalaya
+    x = atalaya_hotel_api_standardization()
+    y = resort_hotel_api_standardization()
+    x['hotels'].extend(y['hotels'])
+    return x
 
 
 
